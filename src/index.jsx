22ba1bd7733608e3940e422e6961fb0439676a5a
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-04-12 16:23:42 +0200

    added _transformCharacter in MarvelService.js and some optimization

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
-// создаем новый экземпляр класса
-const marvelService = new MarvelService();
-marvelService
-    .getAllCharacters()
-    .then((res) => res.data.results.forEach((item) => console.log(item.name)));
-// marvelService.getCharacter(1011052).then((res) => console.log(res));
-
-ReactDOM.render(
-    <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
         <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+    </StrictMode>
 );
