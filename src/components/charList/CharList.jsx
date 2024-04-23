b0fb1d851d289665f8751c16481c9caef8c28364
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-04-23 10:14:31 +0200

    added propTypes

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
+
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -118,5 +120,9 @@ class CharList extends Component {
         );
     }
 }
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func.isRequired,
+};
 
 export default CharList;
