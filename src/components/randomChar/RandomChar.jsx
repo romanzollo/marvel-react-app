63a82001c2b321ab096fc8bf8af0f1f385cc6b48
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-04-29 16:08:53 +0200

    convert class component RandomChar.jsx to functional component

--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
@@ -7,88 +7,74 @@ import mjolnir from '../../resources/img/mjolnir.png';
 
 import './randomChar.scss';
 
-class RandomChar extends Component {
-    state = {
-        char: {},
-        loading: true,
-        error: false,
+const RandomChar = () => {
+    const [char, setChar] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+    const marvelService = new MarvelService();
+
+    const onCharLoaded = (char) => {
+        setChar(char);
+        setLoading(false);
     };
 
-    marvelService = new MarvelService();
-
-    onCharLoaded = (char) => {
-        this.setState({ char, loading: false });
+    const onCharLoading = () => {
+        setLoading((loading) => true);
     };
 
-    onCharLoading = () => {
-        this.setState({
-            loading: true,
-        });
+    const onError = () => {
+        setError((error) => !error);
+        setLoading((loading) => false);
     };
 
-    onError = () => {
-        this.setState({
-            error: !this.state.error,
-            loading: false,
-        });
-    };
-
-    updateChar = () => {
+    const updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
 
-        this.onCharLoading();
+        onCharLoading();
 
-        this.marvelService
-            .getCharacter(id)
-            .then(this.onCharLoaded)
-            .catch(this.onError);
+        marvelService.getCharacter(id).then(onCharLoaded).catch(onError);
     };
 
-    componentDidMount() {
-        this.updateChar();
-    }
+    useEffect(() => {
+        updateChar();
 
-    // componentWillUnmount() {
+        const timerId = setInterval(updateChar, 60000);
 
-    // }
+        // останавливает интервал при размонтировании компонента
+        return () => {
+            clearInterval(timerId);
+        };
+    }, []);
 
-    render() {
-        const { char, loading, error } = this.state;
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error || !char) ? <View char={char} /> : null;
 
-        const errorMessage = error ? <ErrorMessage /> : null;
-        const spinner = loading ? <Spinner /> : null;
-        const content = !(loading || error || !char) ? (
-            <View char={char} />
-        ) : null;
-
-        return (
-            <div className="randomchar">
-                {spinner}
-                {errorMessage}
-                {content}
-                <div className="randomchar__static">
-                    <p className="randomchar__title">
-                        Random character for today!
-                        <br />
-                        Do you want to get to know him better?
-                    </p>
-                    <p className="randomchar__title">Or choose another one</p>
-                    <button
-                        className="button button__main"
-                        onClick={this.updateChar}
-                    >
-                        <div className="inner">try it</div>
-                    </button>
-                    <img
-                        src={mjolnir}
-                        alt="mjolnir"
-                        className="randomchar__decoration"
-                    />
-                </div>
+    return (
+        <div className="randomchar">
+            {spinner}
+            {errorMessage}
+            {content}
+            <div className="randomchar__static">
+                <p className="randomchar__title">
+                    Random character for today!
+                    <br />
+                    Do you want to get to know him better?
+                </p>
+                <p className="randomchar__title">Or choose another one</p>
+                <button className="button button__main" onClick={updateChar}>
+                    <div className="inner">try it</div>
+                </button>
+                <img
+                    src={mjolnir}
+                    alt="mjolnir"
+                    className="randomchar__decoration"
+                />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 // разделил компонент RandomChar на подкомпонент View
 // который отвечает за отображение случайного персонажа
