777ac01504fcae7f1d248e1a039571b5d6867570
Roman Zlagodukhin <zloyandmad@yandex.ru>
2024-05-09 16:37:31 +0200

    added React.lazy and React.Suspense

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,14 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { MainPage, ComicsPage, Page404, SingleComicPage } from '../pages';
 
+import Spinner from '../spinner/Spinner';
 import AppHeader from '../appHeader/AppHeader';
+
+// динамический импорт (lazy) обязательно после всех статических импортов
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+const Page404 = lazy(() => import('../pages/404'));
 
 const App = () => {
     return (
@@ -9,17 +16,19 @@ const App = () => {
             <div className="app">
                 <AppHeader />
                 <main>
-                    <Routes>
-                        <Route path="/" element={<MainPage />} />
-                        <Route path="/comics" element={<ComicsPage />} />
-                        <Route
-                            // comicId это слаг который мы сами придумываем и позже вытащим из адресной строки через useParams в SingleComicPage
-                            path="comics/:comicId"
-                            element={<SingleComicPage />}
-                        />
+                    <Suspense fallback={<Spinner />}>
+                        <Routes>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="/comics" element={<ComicsPage />} />
+                            <Route
+                                // comicId это слаг который мы сами придумываем и позже вытащим из адресной строки через useParams в SingleComicPage
+                                path="comics/:comicId"
+                                element={<SingleComicPage />}
+                            />
 
-                        <Route path="*" element={<Page404 />} />
-                    </Routes>
+                            <Route path="*" element={<Page404 />} />
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
